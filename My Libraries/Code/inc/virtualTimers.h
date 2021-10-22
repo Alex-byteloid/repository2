ff90ffd742892b6e8a96de2fecb2957952e23ed2
Alex-byteloid <rewoluzioner@yandex.ru>
2021-10-22 11:23:57 +0300

    Worked on global timers.
    Писал библиотеку работы с глобальными таймерами.

--- My Libraries/Code/inc/virtualTimers.h
+++ My Libraries/Code/inc/virtualTimers.h
@@ -4,9 +4,27 @@
 /************************* Includes *************************/
 
 #include "stm32f411xe.h"
+#include "system.h"
 
 /*************************	Define	*************************/
 
+#define MaxGTimers 3 // Максимальное количество глобальных таймеров
 
+// Идентификаторы глобальных таймеров
+#define GTimer1	0
+#define GTimer2 1
+#define GTimer3 2
+
+// Состояния таймера
+#define TimerStopped	0	// Таймер остановлен
+#define TimerRunning	1	// Таймер работает
+#define TimerPaused		2	// Таймер на паузе
 
 /*************************	 Code	*************************/
+
+void InitGTimer(void);						// Функция инициализации глобальных таймеров
+void StartGTimer(uint8_t GTimerID);			// Функция зауска выбранного глобального таймера
+void StopGTimer(uint8_t GTimerID);			// Функция остановки выбранного глобального таймера
+void PauseGTimer(uint8_t GTimerID);			// Функция приостановки выбранного глобального таймера
+void ReleaseGTimer(uint8_t GTimerID);		// Функция продолжения счета выбранного глобального таймера
+uint32_t GetGTimerVal(uint8_t GTimerID);	// Функция получения значения выбранного глобального таймера
--- My Libraries/Code/src/virtualTimers.c
+++ My Libraries/Code/src/virtualTimers.c
@@ -8,6 +8,41 @@
 
 /********************* Global Variables *********************/
 
-
+uint8_t GTimerState [MaxGTimers];				// Хранение текущих состояний глобальных таймеров
+uint32_t GTimerVal	[MaxGTimers];				// Хранение текущих значений глобальных таймеров
 
 /*************************	 Code	*************************/
+
+void InitGTimer(void){
+
+	uint8_t i;
+
+	for (i = 0; i < MaxGTimers; i++){
+		GTimerState[i] = TimerStopped;
+	}
+
+}
+
+void StartGTimer(uint8_t GTimerID){
+
+	if (GTimerState[GTimerID] == TimerStopped){
+
+		GTimerVal[GTimerID] = 0;
+		GTimerState[GTimerID] = TimerRunning;
+	}
+
+}
+
+void StopGTimer(uint8_t GTimerID){
+
+	GTimState[GTimerID] = TimerStopped;
+}
+
+void PauseGTimer(uint8_t GTimerID){
+
+	if (GTimerState[GTimerID] == TimerRunning){
+
+		GTimState[GTimerID] = TimerPaused;
+	}
+
+}
